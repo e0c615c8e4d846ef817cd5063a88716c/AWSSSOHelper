44a312abc08c1d64dd72619c5de440754206f9c7
Simon Heather <simonheather99@gmail.com>
2020-06-10 18:44:28 +0100

    Update Comment Based Help

--- AWSSSOHelper.psm1
+++ AWSSSOHelper.psm1
@@ -19,7 +19,9 @@
 .PARAMETER AllAccountRoles
     If specified, credentials for all roles and accounts will be obtained.
 .PARAMETER ClientName
+    The friendly name for the SSO OIDC Client.
 .PARAMETER RefreshAccessToken
+    If specified, the SSO access token will be refreshed.
 .PARAMETER Region
     The system name of an AWS region where the AWS SSO service resides.
 .PARAMETER PassThru
@@ -35,36 +37,69 @@
     If specified, the credentials will be stored in the shell variable $StoredAWSCredentials for use by other AWS
     PowerShell cmdlets.
 .PARAMETER UseProfile
-    If specified then this function will store the credentials as an AWS profile.
+    If specified then this function will store the credentials as an AWS profile in the encrypted credential file used
+    by the AWS SDK for .NET and AWS Toolkit for Visual Studio.
 .PARAMETER UseCliCredentialFile
     If specified then this function will store the credentials in the CLI ini-format credential file at the location
-    specified by the ProfileLocation parameter. Otherwise the function will store the credentials in the encrypted 
-    credential file used by the AWS SDK for .NET and AWS Toolkit for Visual Studio.
+    specified by the ProfileLocation parameter rather than the AWS SDK .NET encrypted credential file.
 .PARAMETER ProfileLocation
     Used to specify the name and location of the ini-format credential file (shared with the AWS CLI and other
     AWS SDKs).
 .EXAMPLE
     Get-AWSSSORoleCredential -StartUrl "https://mycompany.awsapps.com/start"
+
+    Prompt for account and role as applicable and output the credentials as a PSCustomObject.
 .EXAMPLE
     Get-AWSSSORoleCredential -StartUrl "https://mycompany.awsapps.com/start" -AllAccountRoles
+
+    Output all account and role credentials as an array of PSCustomObjects.
 .EXAMPLE
     $RoleCredentials = Get-AWSSSORoleCredential -StartUrl "https://mycompany.awsapps.com/start" -PassThru
     Get-S3Bucket @RoleCredentials
+
+    Prompt for account and role as applicable and output as a hashtable for splatting on an AWS PowerShell cmdlet.
 .EXAMPLE
     $AllRoleCredentials = Get-AWSSSORoleCredential -StartUrl "https://mycompany.awsapps.com/start" -AllAccountRoles
     $AllRoleCredentials | Foreach-Object { Get-S3Bucket -AccessKey $_.AccessKey -SecretKey $_.SecretKey -SessionToken $_.SessionToken }
+
+    Output all account and role credentials as an array of PSCustomObjects and pipe the properties through
+    ForEach-Object to an AWS PowerShell cmdlet.
+.EXAMPLE
+    Get-AWSSSORoleCredential -StartUrl "https://mycompany.awsapps.com/start" -OutputEnvVariables
+    aws s3 ls
+
+    Prompt for account and role as applicable and write to the AWS environment variables for use by the AWS ClI and
+    other compatible tooling (Terraform, Sceptre etc).
+.EXAMPLE
+    Get-AWSSSORoleCredential -StartUrl "https://mycompany.awsapps.com/start" -AllAccountRoles -UseProfile
+    Get-S3Bucket -Profile aws-account-01.SSORole1
+
+    Output all account and role credentials to profiles within the AWS .NET SDK encrypted credential file with a
+    naming convention of <AccountName>.<RoleName>.
+.EXAMPLE
+    Get-AWSSSORoleCredential -StartUrl "https://mycompany.awsapps.com/start" -AllAccountRoles -UseProfile -UseCliCredentialFile
+    aws s3 ls --profile aws-account-01.SSORole1
+
+    Output all account and role credentials to profiles within the AWS CLI credential file with a naming convention of
+    <AccountName>.<RoleName> for use by the AWS CLI.
+
+.EXAMPLE
+    Get-AWSSSORoleCredential -StartUrl "https://mycompany.awsapps.com/start" -AccountId 123456789012 -RoleName SSORole1 -UseStoredAwsCredentials
+    Get-S3Bucket
+
+    Get credentials for the specified account ID and role name and write them to the AWS $StoredAwsCredentials variable
+    for use by AWS PowerShell cmdlets.
+.EXAMPLE
+    $awsCredential = Get-AWSSSORoleCredential -StartUrl "https://mycompany.awsapps.com/start" -AccountId 123456789012 -RoleName SSORole1 -OutputAwsCredential
+    Get-S3Bucket -Credential $awsCredential
+
+    Get credentials for the specified account ID and role name and output them as an Amazon.Runtime.SessionAWSCredentials
+    object for use by AWS PowerShell cmdlets.
 .INPUTS
-    StartUrl (Mandatory)
+    None
 .OUTPUTS
-    AccountId, RoleName, AccessKey, Expiration, SecretKey, SessionToken
-.NOTES
-    General notes
-.COMPONENT
-    The component this cmdlet belongs to
-.ROLE
-    The role this cmdlet belongs to
-.FUNCTIONALITY
-    The functionality that best describes this cmdlet
+    System.Management.Automation.PSObject
+    Amazon.Runtime.SessionAWSCredentials
 #>
 
 function Get-AWSSSORoleCredential {
