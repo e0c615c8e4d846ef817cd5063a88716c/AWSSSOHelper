769c69584b0c98c3ae371d2eaa7770dced8d46cf
Simon Heather <simonheather99@gmail.com>
2020-06-11 15:00:36 +0100

    Standardise quotes.

--- AWSSSOHelper.psm1
+++ AWSSSOHelper.psm1
@@ -48,51 +48,51 @@
     Used to specify the name and location of the ini-format credential file (shared with the AWS CLI and other
     AWS SDKs).
 .EXAMPLE
-    Get-AWSSSORoleCredential -StartUrl "https://mycompany.awsapps.com/start"
+    Get-AWSSSORoleCredential -StartUrl 'https://mycompany.awsapps.com/start'
 
     Prompt for account and role as applicable and output the credentials as a PSCustomObject.
 .EXAMPLE
-    Get-AWSSSORoleCredential -StartUrl "https://mycompany.awsapps.com/start" -AllAccountRoles
+    Get-AWSSSORoleCredential -StartUrl 'https://mycompany.awsapps.com/start' -AllAccountRoles
 
     Output all account and role credentials as an array of PSCustomObjects.
 .EXAMPLE
-    $RoleCredentials = Get-AWSSSORoleCredential -StartUrl "https://mycompany.awsapps.com/start" -PassThru
+    $RoleCredentials = Get-AWSSSORoleCredential -StartUrl 'https://mycompany.awsapps.com/start' -PassThru
     Get-S3Bucket @RoleCredentials
 
     Prompt for account and role as applicable and output as a hashtable for splatting on an AWS PowerShell cmdlet.
 .EXAMPLE
-    $AllRoleCredentials = Get-AWSSSORoleCredential -StartUrl "https://mycompany.awsapps.com/start" -AllAccountRoles
+    $AllRoleCredentials = Get-AWSSSORoleCredential -StartUrl 'https://mycompany.awsapps.com/start' -AllAccountRoles
     $AllRoleCredentials | Foreach-Object { Get-S3Bucket -AccessKey $_.AccessKey -SecretKey $_.SecretKey -SessionToken $_.SessionToken }
 
     Output all account and role credentials as an array of PSCustomObjects and pipe the properties through
     ForEach-Object to an AWS PowerShell cmdlet.
 .EXAMPLE
-    Get-AWSSSORoleCredential -StartUrl "https://mycompany.awsapps.com/start" -OutputEnvVariables
+    Get-AWSSSORoleCredential -StartUrl 'https://mycompany.awsapps.com/start' -OutputEnvVariables
     aws s3 ls
 
     Prompt for account and role as applicable and write to the AWS environment variables for use by the AWS ClI and
     other compatible tooling (Terraform, Sceptre etc).
 .EXAMPLE
-    Get-AWSSSORoleCredential -StartUrl "https://mycompany.awsapps.com/start" -AllAccountRoles -UseProfile
+    Get-AWSSSORoleCredential -StartUrl 'https://mycompany.awsapps.com/start' -AllAccountRoles -UseProfile
     Get-S3Bucket -Profile aws-account-01.SSORole1
 
     Output all account and role credentials to profiles within the AWS .NET SDK encrypted credential file with a
     naming convention of <AccountName>.<RoleName>.
 .EXAMPLE
-    Get-AWSSSORoleCredential -StartUrl "https://mycompany.awsapps.com/start" -AllAccountRoles -UseProfile -UseCliCredentialFile
+    Get-AWSSSORoleCredential -StartUrl 'https://mycompany.awsapps.com/start' -AllAccountRoles -UseProfile -UseCliCredentialFile
     aws s3 ls --profile aws-account-01.SSORole1
 
     Output all account and role credentials to profiles within the AWS CLI credential file with a naming convention of
     <AccountName>.<RoleName> for use by the AWS CLI.
 
 .EXAMPLE
-    Get-AWSSSORoleCredential -StartUrl "https://mycompany.awsapps.com/start" -AccountId 123456789012 -RoleName SSORole1 -UseStoredAwsCredentials
+    Get-AWSSSORoleCredential -StartUrl 'https://mycompany.awsapps.com/start' -AccountId 123456789012 -RoleName SSORole1 -UseStoredAwsCredentials
     Get-S3Bucket
 
     Get credentials for the specified account ID and role name and write them to the AWS $StoredAwsCredentials variable
     for use by AWS PowerShell cmdlets.
 .EXAMPLE
-    $awsCredential = Get-AWSSSORoleCredential -StartUrl "https://mycompany.awsapps.com/start" -AccountId 123456789012 -RoleName SSORole1 -OutputAwsCredential
+    $awsCredential = Get-AWSSSORoleCredential -StartUrl 'https://mycompany.awsapps.com/start' -AccountId 123456789012 -RoleName SSORole1 -OutputAwsCredential
     Get-S3Bucket -Credential $awsCredential
 
     Get credentials for the specified account ID and role name and output them as an Amazon.Runtime.SessionAWSCredentials
@@ -139,6 +139,8 @@ function Get-AWSSSORoleCredential {
         [switch]$PassThru,
 
         [Parameter()]
+        [string]$ClientName = 'default',
+
         [Parameter()]
         [ValidateSet('Public')]
         [string]$ClientType = 'Public',
@@ -147,7 +149,7 @@ function Get-AWSSSORoleCredential {
         [int]$TimeoutInSeconds = 120,
 
         [Parameter()]
-        [string]$Path = (Join-Path $Home ".awsssohelper"),
+        [string]$Path = (Join-Path $Home '.awsssohelper'),
 
         [Parameter(ParameterSetName = 'OutputAwsCredential')]
         [switch]$OutputAwsCredential,
@@ -207,7 +209,7 @@ function Get-AWSSSORoleCredential {
                 -Credential ([Amazon.Runtime.AnonymousAWSCredentials]::new()) -Verbose:$false | Out-Null
         }
         catch {
-            Write-Host "Cached access token is no longer valid, will need to obtain via SSO."
+            Write-Host 'Cached access token is no longer valid, will need to obtain via SSO.'
             $RefreshAccessToken = $true
         }
     }
@@ -240,7 +242,7 @@ function Get-AWSSSORoleCredential {
         }
         
         Clear-Variable AccessToken -ErrorAction SilentlyContinue
-        Write-Host "Waiting for SSO login via browser..."
+        Write-Host 'Waiting for SSO login via browser...'
         $SSOStart = Get-Date
         
         while (!$AccessToken -and ((New-TimeSpan $SSOStart (Get-Date)).TotalSeconds -lt $TimeoutInSeconds)) {
@@ -250,7 +252,7 @@ function Get-AWSSSORoleCredential {
                     ClientSecret = $Client.ClientSecret
                     Code         = $DeviceAuth.Code
                     DeviceCode   = $DeviceAuth.DeviceCode
-                    GrantType    = "urn:ietf:params:oauth:grant-type:device_code"
+                    GrantType    = 'urn:ietf:params:oauth:grant-type:device_code'
                     Credential   = ([Amazon.Runtime.AnonymousAWSCredentials]::new())
                 }
                 $AccessToken = New-SSOOIDCToken @newSSOIDCTokenParms
@@ -273,13 +275,13 @@ function Get-AWSSSORoleCredential {
             -Credential ([Amazon.Runtime.AnonymousAWSCredentials]::new()) -Verbose:$false
     }
     catch {
-        throw ("Error obtaining account list, access token is invalid.  Try running the command again with " +
+        throw ('Error obtaining account list, access token is invalid.  Try running the command again with ' +
             "'-RefreshAccessToken' parameter.")
     }
 
     if (!$AccountId) {
         if (!$AllAccountRoles) {
-            $outGridViewParms = @{ }
+            $outGridViewParms = @{}
             if ($UseStoredAwsCredentials) {
                 $outGridViewParms.Title = 'Select AWS Account'
                 $outGridViewParms.OutputMode = 'Single'
@@ -330,7 +332,7 @@ function Get-AWSSSORoleCredential {
                 Set-AWSCredential @setAwsCredentialParms
             }
             elseif ($UseStoredAwsCredentials) {
-                Write-Verbose -Message "Storing the credentials in the StoredAwsCredentials Global Variable"
+                Write-Verbose -Message 'Storing the credentials in the $StoredAwsCredentials Global Variable'
                 $setAwsCredentialParms = @{
                     AccessKey    = $credential.AccessKey
                     SecretKey    = $credential.SecretKey
@@ -347,7 +349,7 @@ function Get-AWSSSORoleCredential {
                 $env:AWS_SESSION_TOKEN = $credential.SessionToken
             }
             else {
-                Write-Verbose -Message "Outputting the Credentials as a PSCustomObject"
+                Write-Verbose -Message 'Outputting the Credentials as a PSCustomObject'
                 $credential
             }
         }
@@ -372,7 +374,7 @@ function GetAccountRoleCredential {
             $AccountRoles = ($SSORoles | Select-Object -First 1).RoleName
         }
         elseif (!$AllAccountRoles) {
-            $AccountRoles = ($SSORoles | Out-GridView -PassThru -Title "Select AWS SSO Role").RoleName
+            $AccountRoles = ($SSORoles | Out-GridView -PassThru -Title 'Select AWS SSO Role').RoleName
         }
         else {
             $AccountRoles = $SSORoles.RoleName
